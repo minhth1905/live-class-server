10709b53a4272b434d1383fdfb60e511f9388829
Tran Huu Minh <minhth1905@gmail.com>
2017-09-17 13:33:00 +0700

    init

---
+++ public/build/ng-aruco-marker.js
@@ -0,0 +1,37 @@
+/*! aruco-marker 2.0.0 2017-09-15 - MIT Licensed, see http://github.com/bhollis/aruco-marker */
+// AngularJS Directive for Aruco marker images
+// Usage:
+// <aruco-marker marker-id="{{markerId}}" size="500px"/>
+(function (root, factory) {
+  if (typeof module !== 'undefined' && module.exports) {
+    // CommonJS
+    if (typeof angular === 'undefined') {
+      factory(require('angular'));
+    } else {
+      factory(angular);
+    }
+    module.exports = 'ngDialog';
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(['angular'], factory);
+  } else {
+    // Global Variables
+    factory(root.angular);
+  }
+}(this, function (angular) {
+  angular.module('arucoMarker', []).directive('arucoMarker', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        markerId: '@',
+        size: '@'
+      },
+      link: function(scope, element, attrs) {
+        scope.$watch('markerId', function(newVal, oldVal) {
+          var marker = new ArucoMarker(+scope.markerId);
+          element.html(marker.toSVG(scope.size));
+        });
+      }
+    };
+  });
+}));
